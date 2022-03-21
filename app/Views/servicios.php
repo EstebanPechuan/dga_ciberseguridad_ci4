fd6241696f3b9dd3c30fd6559e76ffb0e1b141cb
EstebanPechuan <esteban.pechuan@gmail.com>
2022-03-21 20:35:20 -0300

    Correcciones Ciberseguridad

--- app/Views/servicios.php
+++ app/Views/servicios.php
@@ -9,11 +9,11 @@
 
 
     <section class="links-utiles-servicios">
-        <h3>Trámites / Servicios</h3>
+        <h3>Servicios</h3>
 
         <div class="link">
             <a href="#c">
-                <h4>Validación de cambios</h4>
+                <h4>Análisis de Vulnerabilidades</h4>
                 <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
                 <!-- <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
@@ -23,7 +23,7 @@
 
         <div class="link">
             <a href=" <?= base_url('servicios/sistemas') ?> ">
-                <h4>Auditoría y desbloqueo de usuarios</h4>
+                <h4>Auditoria de Incidentes Tecnológicos</h4>
                 <!-- <p>Requerimiento de nuevo desarrollo. (link a formulario de requerimiento en elaboración)</p> -->
                 <!-- <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
@@ -33,7 +33,7 @@
 
         <div class="link">
             <a href="#c">
-                <h4>Auditoría de incidentes</h4>
+                <h4>Servicios de Protección de Punto Final</h4>
                 <!-- <p>Requerimiento de nuevo desarrollo. (link a formulario de requerimiento en elaboración)</p> -->
                 <!-- <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
@@ -43,7 +43,7 @@
 
         <div class="link">
             <a href="#c">
-                <h4>Análisis de vulnerabilidades</h4>
+                <h4>Gestión de Seguridad de Página Web y Correo Electrónico</h4>
                 <!-- <p>Iniciar ticket de Soporte</p> -->
                 <!-- <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
@@ -53,7 +53,7 @@
 
         <div class="link">
             <a href="#c">
-                <h4>Validación de excepciones de seguridad sobre activos tecnológicos</h4>
+                <h4>Concientización y Comunicación</h4>
                 <!-- <p>Decreto 1254</p> -->
                 <!-- <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
@@ -63,7 +63,61 @@
 
         <div class="link">
             <a href="#c">
-                <h4>EPP</h4>
+                <h4>Generación de Políticas de Seguridad</h4>
+                <!-- <p>Decreto 1254</p> -->
+                <!-- <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5> -->
+            </a>
+        </div>
+    </section>
+
+    <section class="links-utiles-servicios">
+        <h3>Infraestructura</h3>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Creación y Gestión de Servidores Virtuales</h4>
+                <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
+                <!-- <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5> -->
+            </a>
+        </div>
+
+        <div class="link">
+            <a href=" <?= base_url('servicios/sistemas') ?> ">
+                <h4>Monitoreo y Relevamiento de Servicios</h4>
+                <!-- <p>Requerimiento de nuevo desarrollo. (link a formulario de requerimiento en elaboración)</p> -->
+                <!-- <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5> -->
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Housing de Servidores Físicos</h4>
+                <!-- <p>Requerimiento de nuevo desarrollo. (link a formulario de requerimiento en elaboración)</p> -->
+                <!-- <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5> -->
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Gestión de Correo Electronico</h4>
+                <!-- <p>Iniciar ticket de Soporte</p> -->
+                <!-- <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5> -->
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Sistema de Archivo Centralizado para los Sistemas Integrados</h4>
                 <!-- <p>Decreto 1254</p> -->
                 <!-- <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
@@ -73,7 +127,7 @@
 
         <div class="link">
             <a href="#c">
-                <h4>Gestión de información y eventos de seguridad</h4>
+                <h4>Aprovisionamiento de Escritorio Remoto para Teletrabajo</h4>
                 <!-- <p>Decreto 1254</p> -->
                 <!-- <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
@@ -83,7 +137,17 @@
 
         <div class="link">
             <a href="#c">
-                <h4>Definición y cumplimiento de políticas de ciberseguridad</h4>
+                <h4>Gestión de Servicio de Directorio (Active Directory)</h4>
+                <!-- <p>Decreto 1254</p> -->
+                <!-- <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5> -->
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Administración de Base de Datos Clusterizadas</h4>
                 <!-- <p>Decreto 1254</p> -->
                 <!-- <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
@@ -98,7 +162,77 @@
 
         <div class="link">
             <a href="#c">
-                <h4>Catálogo de Aplicaciones y Servicios</h4>
+                <h4>Validación de Cambios Tecnológicos (F012) - Presentado por Mail</h4>
+                <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
+                <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5>
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Auditoria y Desbloqueo de Usuarios de los Sistemas Integrados (F103) -  Presentado por soporte.sanjuan.gob.ar o mail</h4>
+                <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
+                <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5>
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Creación de Servidor Virtual (F130) - Presentado por soporte.sanjuan.gob.ar</h4>
+                <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
+                <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5>
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Creación de Usuario de Servicio Directorio (F201) - Presentado por soporte.sanjuan.gob.ar</h4>
+                <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
+                <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5>
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Solicitud de Correo Electrónico (F202) - Presentado por soporte.sanjuan.gob.ar o mail</h4>
+                <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
+                <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5>
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Reporte de Incidente (F204) - Presentado por soporte.sanjuan.gob.ar</h4>
+                <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
+                <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5>
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Solicitud para Trabajo Remoto (F205) - Presentado por soporte.sanjuan.gob.ar</h4>
+                <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
+                <h5 href="#c">
+                    Página Web <i class='bx bx-world'></i>
+                </h5>
+            </a>
+        </div>
+
+        <div class="link">
+            <a href="#c">
+                <h4>Validación de Excepciones de Seguridad de Sitios Web - Presentado por mail</h4>
                 <!-- <p>Requerimiento sueldos (Iniciar expediente SIGED desde CIDI)</p> -->
                 <h5 href="#c">
                     Página Web <i class='bx bx-world'></i>
